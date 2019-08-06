2b6957b55cc0c2e7c6e001fd0676d8eb2e8540b1
SeigiroSpica <SeigiroSpica>
2019-08-06 12:06:42 -0300

    Version 1.0.0

--- src/tarea0608/Tarea0608.java
+++ src/tarea0608/Tarea0608.java
@@ -23,12 +23,14 @@ public class Tarea0608 {
         int opcion = 0;
         String datoGuardado;
         Electrodomestico item;
-        while (opcion < 4) {
+        while (opcion < 6) {
             System.out.println("   MENU PRINCIPAL");
             System.out.println("1- Añadir TV.");
             System.out.println("2- Añadir Lavadora.");
             System.out.println("3- Añadir Otro Elctrodomestico.");
-            System.out.println("4- Mostrar Precio Final de todos los aticulos.");
+            System.out.println("4- Mostrar Precio de venta de cada articulo.");
+            System.out.println("5- Mostrar valor total de todos los aticulos.");
+            System.out.println("6- Salir.");
             System.out.print("\nOpción: ");
             opcion = Integer.parseInt(teclado.nextLine());
             switch (opcion) {
@@ -45,20 +47,21 @@ public class Tarea0608 {
                     peso = Double.parseDouble(teclado.nextLine());
                     System.out.print("Ingrese el color(Blanco, Negro, Rojo, Azul, Gris): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    color=Enum.valueOf(ColoresDisponibles.class, datoGuardado);
+                    color = Enum.valueOf(ColoresDisponibles.class, datoGuardado);
                     System.out.print("Ingrese el Consumo Energetico(A-F): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    consumoE= Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
+                    consumoE = Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
                     System.out.print("Ingrese la resolucion(en pulgadas): ");
                     resolucion = Integer.parseInt(teclado.nextLine());
                     System.out.print("sintonizador TDT(Si/No): ");
-                    datoGuardado= teclado.nextLine().toUpperCase();
+                    datoGuardado = teclado.nextLine().toUpperCase();
                     TDT = datoGuardado.equals("SI");
-                    item= new Television(resolucion, TDT, precio, peso, color, consumoE);
+                    item = new Television(resolucion, TDT, precio, peso, color, consumoE);
                     electrodomestico.add(item);
                     System.out.println("\nNuevo TV añadido.\n\nPresione Enter para continuar...");
                     teclado.nextLine();
-                } break;
+                }
+                break;
                 case 2: {
                     double precio, peso, tambor;
                     ColoresDisponibles color;
@@ -70,17 +73,18 @@ public class Tarea0608 {
                     peso = Double.parseDouble(teclado.nextLine());
                     System.out.print("Ingrese el color(Blanco, Negro, Rojo, Azul, Gris): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    color=Enum.valueOf(ColoresDisponibles.class, datoGuardado);
+                    color = Enum.valueOf(ColoresDisponibles.class, datoGuardado);
                     System.out.print("Ingrese el Consumo Energetico(A-F): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    consumoE= Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
+                    consumoE = Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
                     System.out.print("Ingrese la tamaño de tambor (kg): ");
                     tambor = Double.parseDouble(teclado.nextLine());
-                    item= new Lavadora(tambor, precio, peso, color, consumoE);
+                    item = new Lavadora(tambor, precio, peso, color, consumoE);
                     electrodomestico.add(item);
                     System.out.println("\nNueva Lavadora añadida.\n\nPresione Enter para continuar...");
                     teclado.nextLine();
-                } break;
+                }
+                break;
                 case 3: {
                     double precio, peso, tambor;
                     ColoresDisponibles color;
@@ -92,24 +96,65 @@ public class Tarea0608 {
                     peso = Double.parseDouble(teclado.nextLine());
                     System.out.print("Ingrese el color(Blanco, Negro, Rojo, Azul, Gris): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    color=Enum.valueOf(ColoresDisponibles.class, datoGuardado);
+                    color = Enum.valueOf(ColoresDisponibles.class, datoGuardado);
                     System.out.print("Ingrese el Consumo Energetico(A-F): ");
                     datoGuardado = teclado.nextLine().toUpperCase();
-                    consumoE= Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
-                    item= new Electrodomestico(precio, peso, color, consumoE);
+                    consumoE = Enum.valueOf(ConsumoEnergetico.class, datoGuardado);
+                    item = new Electrodomestico(precio, peso, color, consumoE);
                     electrodomestico.add(item);
                     System.out.println("\nNuevo Electrodomestico Añadido.\n\nPresione Enter para continuar...");
                     teclado.nextLine();
-                } break;
+                }
+                break;
                 case 4: {
-                    for (int i=0;i<electrodomestico.size();i++){
-                        System.out.println(electrodomestico.get(i).precioFinal());
-
+                    for (int i = 0; i < electrodomestico.size(); i++) {
+                        if (electrodomestico.get(i) instanceof Television) {
+                            System.out.println("TV €" + electrodomestico.get(i).precioFinal());
+                        } else {
+                            if (electrodomestico.get(i) instanceof Lavadora) {
+                                System.out.println("Lavadora €" + electrodomestico.get(i).precioFinal());
+                            } else {
+                                System.out.println("Electrodomestico X €" + electrodomestico.get(i).precioFinal());
+                            }
+                        }
                     }
-                } break;
+                    System.out.println("\n\nPresione Enter para continuar...");
+                    teclado.nextLine();
+                }
+                break;
+                case 5: {
+                    int totalLavadoras = 0,
+                            totalTV = 0,
+                            totalElectrodomesticos = 0;
+                    for (int i = 0; i < electrodomestico.size(); i++) {
+                        if (electrodomestico.get(i) instanceof Television) {
+                            totalTV += electrodomestico.get(i).precioFinal();
+                        } else {
+                            if (electrodomestico.get(i) instanceof Lavadora) {
+                                totalLavadoras += electrodomestico.get(i).precioFinal();
+                            } else {
+                                totalElectrodomesticos += electrodomestico.get(i).precioFinal();
+                            }
+                        }
+                    }
+                    System.out.println("Valor total de TV: €" + totalTV + ".");
+                    System.out.println("Valor total de Lavadoras: €" + totalLavadoras + ".");
+                    System.out.println("Valor total de Electrodomesticos: €" + totalElectrodomesticos + ".");
+                    System.out.println("\n\nPresione Enter para continuar...");
+                    teclado.nextLine();
+                }
+                break;
+                case 6: 
+                    break;
+                default: {
+                    opcion = 0;
+                    System.out.println("\nOpción no valida.");
+                    System.out.println("\n\nPresione Enter para volver al menu...");
+                    teclado.nextLine();
+                }
+                break;
             }
         }
     }
 
 }
-
