0c11926ff713c710e345b5e39872d812b5af3016
Desarrollo Web <Desarrollo Web@cpu2422>
2019-08-08 13:53:32 -0300

    Version 1.0.3

--- src/tarea0608/Electrodomestico.java
+++ src/tarea0608/Electrodomestico.java
@@ -15,6 +15,9 @@ public class Electrodomestico {
     private ConsumoEnergetico consumoE;
     private PesoPrecio pesoRango;
 
+    /**
+     *  Constructor por default.
+     */
     public Electrodomestico() {
         precioBase= 100; //Euros
         peso= 5; //kg
@@ -23,6 +26,12 @@ public class Electrodomestico {
         consumoE= ConsumoEnergetico.F;
     }
     
+    /**
+     * Constructor que recibe el el peso y precio base, ambos como un double
+     * @param precioBase recibe el precio base del producto como un Doble.
+     * @param peso recibe el peso del producto como un double (en kg)
+     */
+    
     public Electrodomestico(double precioBase, double peso) {
         this.precioBase = precioBase;
         this.peso = peso;
@@ -30,6 +39,14 @@ public class Electrodomestico {
         color= ColoresDisponibles.BLANCO;
         consumoE= ConsumoEnergetico.F;
     }
+    
+    /**
+     * Constructor que recibe el el peso y precio base como doble, y el color como Enum de Colores disponibles y el Consumo energetico como enumo de ConsumoEnergetico
+     * @param precioBase recibe el precio base del producto como un Doble.
+     * @param peso recibe el peso del producto como un double (en kg)
+     * @param color recibe el color como Enum (tenìa que ser como caracter, hay que cambiar)
+     * @param consumoE recibe el consumo energetico como Enum (tenìa que ser como caracter, hay que cambiar)
+     */
 
     public Electrodomestico(double precioBase, double peso, ColoresDisponibles color, ConsumoEnergetico consumoE) {
         this.precioBase = precioBase;
@@ -38,7 +55,11 @@ public class Electrodomestico {
         this.color = comprobarColor(color);
         this.consumoE = comprobarConsumoEnergetico(consumoE);
     }
-    
+    /**
+     * Este metodo precibe el Peso del producto, y devuelve el Enum que corresponda a dicho peso
+     * @param peso recibe el peso del producto como un double (en kg)
+     * @return Devuelve el Enum de PesoPrecio que corresponda.
+     */
     private PesoPrecio darRangoPeso(double peso){
         if (peso>=80){
             return PesoPrecio.XL;
@@ -54,6 +75,8 @@ public class Electrodomestico {
             }
         }
     }
+    
+    
     private ConsumoEnergetico comprobarConsumoEnergetico(ConsumoEnergetico consumoE){
         for (ConsumoEnergetico i: ConsumoEnergetico.values()){
             if (i==consumoE){
